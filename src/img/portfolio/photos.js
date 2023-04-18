d8cb95713d2e709f88c43cceee658bfe2a67c46f
Ravin Io <rio@onestaffmedical.com>
2023-04-18 13:06:26 -0500

    images

---
+++ src/img/portfolio/photos.js
@@ -0,0 +1,19 @@
+import img1 from "./reels/"
+
+export const photos = [
+    {
+      src: "../",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      width: 3,
+      height: 4
+    },
+];
